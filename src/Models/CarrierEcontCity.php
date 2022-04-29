4b6818cae01f910a0d3a97c2d9239bb4760c01e2
gdinko <gdinko@users.noreply.github.com>
2022-04-29 17:06:32 +0000

    Fix styling

--- src/Models/CarrierEcontCity.php
+++ src/Models/CarrierEcontCity.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class CarrierEcontCity extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'econt_city_id',
         'country_code3',
