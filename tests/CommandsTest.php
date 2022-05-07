ade242b1fe1c00ef5fa1e72587b59927c31debf1
Dinko Georgiev <dinko359@gmail.com>
2022-05-07 17:57:45 +0300

    tests fix

--- tests/CommandsTest.php
+++ tests/CommandsTest.php
@@ -1,17 +1,17 @@
 <?php
 
 it('Can Sync Countries', function () {
-    $this->artisan('sync:carrier-econt-countries')->assertExitCode(0);
+    $this->artisan('econt:sync-countries')->assertExitCode(0);
 });
 
 it('Can Sync Offices', function () {
-    $this->artisan('sync:carrier-econt-offices')->assertExitCode(0);
+    $this->artisan('econt:sync-offices')->assertExitCode(0);
 });
 
 it('Can Sync Quarters', function () {
-    $this->artisan('sync:carrier-econt-quarters')->assertExitCode(0);
+    $this->artisan('econt:sync-quarters')->assertExitCode(0);
 });
 
 it('Can Check API Status', function () {
-    $this->artisan('get:carrier-econt-api-status')->assertExitCode(0);
+    $this->artisan('econt:api-status')->assertExitCode(0);
 });
